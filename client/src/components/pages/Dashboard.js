57ac6fc963fc5908471dbe857397755c4eddf183
Sowmya Nagayya <kkd.sowmya@gmail.com>
2021-11-23 14:32:38 -0600

    updated variable and function name

--- client/src/components/pages/Dashboard.js
+++ client/src/components/pages/Dashboard.js
@@ -1,11 +1,23 @@
 import React, {useState, useEffect } from "react";
 
 export default function Dashboard(props) {
+
+    const createGroupclick = () => {
+       
+        window.location.href="/newgroup";
+      //  <Signup/>
+        alert("Hello");
+        // return(
+            
+        // <Signup />
+        // );
+    }
+
     return (
         <>
             <div className="row">
-                <button type="button" class="btn btn-success">Create New Chat</button>
-                {props.groups.map(group => (
+                <button type="button" class="btn btn-success" onClick={createGroupclick}>Create New Chat</button>
+                {/* {props.groups.map((group) => (
                     <div class="col-sm-6">
                         <div class="card" key={group.id}>
                             <div class="card-body">
@@ -15,7 +27,7 @@ export default function Dashboard(props) {
                             </div>
                         </div>
                     </div>
-                ))}
+                ))} */}
             </div>
         </>
     )
