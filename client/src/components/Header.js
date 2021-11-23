5bd7b94bbb977a64791a4d7e89191f8c7820a00d
Sowmya Nagayya <kkd.sowmya@gmail.com>
2021-11-22 18:12:34 -0600

    updated signup page component added in header component

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,10 +1,11 @@
 import React, {useState, useEffect } from "react";
+import Signup from "./Signup";
 
 export default function Header() {
 
     return (
         <div>
-            
+           <Signup /> 
         </div>
         
     )
