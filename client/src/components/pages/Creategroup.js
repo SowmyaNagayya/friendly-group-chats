91d93653d0d224bd96c64f122df036078c9733a0
Sowmya Nagayya <kkd.sowmya@gmail.com>
2021-11-26 10:57:25 -0600

    github issues

--- client/src/components/pages/Creategroup.js
+++ client/src/components/pages/Creategroup.js
@@ -1,94 +1,112 @@
 import React, {useState, useEffect } from "react";
-import { fetchUsers } from '../../utils/api';
-import { Form, Select, Button } from 'react-bootstrap';
+import { fetchUsers ,createGroup} from '../../utils/api';
+import { Form,Select, Button } from 'react-bootstrap';
 
 // import {userLogin} from '../utils/api';
 // import Auth from '../utils/auth';
 
 export default function Creategroup() {
-  const [ createGroup, setCreateGroup ] = useState("");
-
-  const [ allUsers, setAllUsers ] = useState([])
-
+  const [createGroupName, setCreateGroupName] = useState("");
+  const [allUsers, setAllUsers]=useState([])
+  const [selectedUsers, setSelectedUsers]=useState([])
+  const [validated] = useState(false);
   const options =null;
   useEffect( async () => {
     let userFetch = await fetchUsers();
-    let userFetchData = await userFetch.json();
+    let userFetchData = await userFetch.json();    
 
+    // This var should let us have a running list of all the users in the database
     console.log(JSON.stringify(userFetchData) + "Hello");
-
-    let options = userFetchData.map((item) => {
+    let options =userFetchData.map((item) => {
       return (
-        <option key={item._id} value={item.username}>{item.username}
+        <option key={item._id} value={item._id}>{item.username}
         </option>
       )
     })
-
     setAllUsers(options)
-
-    console.log(JSON.stringify(options) + " what options")
-    
-   // state(state => ({ ...state, allUsers:options }));
-    
-    //setAllUsers({allUsers, userFetchData} )
-  })
+    //console.log(JSON.stringify(options) + " what options")
+       
+  } ,[])
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
-    setCreateGroup({ ...createGroup, [name]: value });
+    setCreateGroupName({ ...createGroupName, [name]: value });
   };
 
-  // const options = userFetchData.map((item) => {
-  //   return (
-  //     <option key={item.id} value={item.username}>
-  //       {item.username}
-  //     </option>
-  //   )
-  // })
+  
+  const handleFormSubmit = async (event) => {
+   
+    event.preventDefault();
+    const formGroup = event.currentTarget; 
+    console.log("here" + createGroupName.groupname + selectedUsers );
 
-  // const handleFormSubmit = async (event) => {
-  //   event.preDefault();
+    if(formGroup.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
 
-  //   const formGroup = event.currentTarget; 
-  //   if(formGroup.checkValidity() === false) {
-  //     event.preventDefault();
-  //     event.stopPropagation();
-  //   }
-  // }
+    try {
+      var name = JSON.stringify(createGroupName.groupname);
+      var users = selectedUsers;
+      
+      var groupData = {name,users}
+     
+      const data = await createGroup( {variables:{...groupData}})
+      alert("Submitted");
+     
+
+    } catch (e) {
+      console.log("Error")
+      alert(e);
+    }
+  }
+
+  const handleUserChange=  (event) => {
+    var options = event.target.options;
+    var value = [];
+    for (var i = 0, l = options.length; i < l; i++) {
+      if (options[i].selected) {
+        value.push(options[i].value);
+      }
+    }
+
+    setSelectedUsers(value);
+    //alert(value + "my selected values")
+  }
+
+  
+
+    
 
     return (
-      <Form>
-      <Form.Group>
+      <Form  noValidate validated={validated} onSubmit={handleFormSubmit}>
+      <Form.Group >
           <Form.Label htmlFor="groupname">Groupname</Form.Label>
           <Form.Control
             type="text"
             placeholder="Your Groupname"
             name="groupname"
             onChange={handleInputChange}
-            value={createGroup.name}
+            value={createGroupName.name}
             required
           />
-              <Form.Control as="select" multiple>
-                {allUsers}
-              </Form.Control>
-              {/* <Form.Select
-                data={userFetchData.map((item) =>item.username)}
-                selectMultiple={true}
-                touchUi={false}
-              /> */}
-            </Form.Group>
-            <Button
-              // disabled={!(createGroup.name && allUsers)}
-              type="submit"
-              variant="success"
-            >
+              
+          <Form.Label htmlFor="addfriend">Add Your Friend To Group</Form.Label>
+          <Form.Control as="select" multiple onChange={handleUserChange}>
+            {allUsers}
+          </Form.Control>
+                {/* <Form.Select
+                  data={userFetchData.map((item) =>item.username)}
+                  selectMultiple={true}
+                  touchUi={false}
+                /> */}
+     </Form.Group>
+          <Button
+          //  disabled={!(createGroup.name && allUsers.username)}
+            type="submit"
+            variant="success" >
               Create Group
-            </Button>
-            </Form>
-          
-        
-       
-
-        
+          </Button>
+      </Form> 
     )
 }
