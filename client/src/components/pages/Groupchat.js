850c5cfd86cb213a5a3aaf319cb93399a01d7b1a
erikaosterbur <erikaosterbur@gmail.com>
2021-11-28 18:21:01 -0600

    user and group connected to chat

--- client/src/components/pages/Groupchat.js
+++ client/src/components/pages/Groupchat.js
@@ -105,13 +105,6 @@ export default function Groupchat(props) {
     const createChatClick = ( e, req ) => {
         //  Prevent page reload
         e.preventDefault();
-
-        //  This is logging exactly what we need just putting it in the database everytime a button is hit.
-        console.log(input);
-
-        //  TODO: get final value of input for the body, we still need a way to get group id and user id then use createChat to submit that to database and set form empty again.
-        
-        //Figure out how to pass the group id in - can pass in as object?
         createChat({input, id});
     }
 
