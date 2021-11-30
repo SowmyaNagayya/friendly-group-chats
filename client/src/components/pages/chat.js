ad242dc79cfa35e311ea5f4c012df54ddb939807
Sowmya Nagayya <kkd.sowmya@gmail.com>
2021-11-29 19:19:16 -0600

    working on chat4

--- client/src/components/pages/chat.js
+++ client/src/components/pages/chat.js
@@ -21,35 +21,35 @@ export default function Example() {
         type: 'text',
         timestamp: 1578366393250,
       },
-      {
-        author: { username: 'user2', id: 2, avatarUrl: null },
-        text: 'Show two buttons',
-        type: 'text',
-        timestamp: 1578366425250,
-        buttons: [
-          {
-            type: 'URL',
-            title: 'Yahoo',
-            payload: 'http://www.yahoo.com',
-          },
-          {
-            type: 'URL',
-            title: 'Example',
-            payload: 'http://www.example.com',
-          },
-        ],
-      },
-      {
-        author: {
-          username: 'user1',
-          id: 1,
-          avatarUrl: 'https://image.flaticon.com/icons/svg/2446/2446032.svg',
-        },
-        text: "What's up?",
-        type: 'text',
-        timestamp: 1578366425250,
-        hasError: true,
-      },
+    //   {
+    //     author: { username: 'user2', id: 2, avatarUrl: null },
+    //     text: 'Show two buttons',
+    //     type: 'text',
+    //     timestamp: 1578366425250,
+    //     buttons: [
+    //       {
+    //         type: 'URL',
+    //         title: 'Yahoo',
+    //         payload: 'http://www.yahoo.com',
+    //       },
+    //       {
+    //         type: 'URL',
+    //         title: 'Example',
+    //         payload: 'http://www.example.com',
+    //       },
+    //     ],
+    //   },
+    //   {
+    //     author: {
+    //       username: 'user1',
+    //       id: 1,
+    //       avatarUrl: 'https://image.flaticon.com/icons/svg/2446/2446032.svg',
+    //     },
+    //     text: "What's up?",
+    //     type: 'text',
+    //     timestamp: 1578366425250,
+    //     hasError: true,
+    //   },
     ],
   });
   const handleClickIcon = () => {
