5491557dc33e589db5f1c1a6f70661fe7f5ff1eb
erikaosterbur <erikaosterbur@gmail.com>
2021-12-01 12:37:58 -0600

    change in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Container from "./components/Container";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.scss';
+import './App.css';
 
 const App = () => <Container />;
 
