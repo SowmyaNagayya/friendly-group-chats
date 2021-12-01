b0e93e4187692e5ae4c607c3163a5526a05bf69d
Sowmya Nagayya <kkd.sowmya@gmail.com>
2021-12-01 13:19:47 -0600

    Added wildcard route

--- server/server.js
+++ server/server.js
@@ -31,6 +31,8 @@ app.use(session(sess));
 app.use(express.static(path.join(__dirname, '../client/build')));
 app.use(routes);
 
+app.use('/*', express.static(path.join(__dirname, '../client/build/index.html')))
+
 db.once('open', () => {
   app.listen(PORT, () => console.log(`Now listening on localhost: ${PORT}`));
 });
